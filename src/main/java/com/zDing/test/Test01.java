e5f7ffc1f329cd6714c1e907ff91e95246961b72
luojunping <2839132232@qq.com>
2022-10-22 08:59:36 +0800

    test01 第二次修改

--- src/main/java/com/zDing/test/Test01.java
+++ src/main/java/com/zDing/test/Test01.java
@@ -3,6 +3,7 @@ package com.zDing.test;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git2!!!");
     }
 
 }
