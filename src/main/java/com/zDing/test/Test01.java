d18cca3cbc4241536f690358017050d946f94360
luojunping <2839132232@qq.com>
2022-10-22 09:05:02 +0800

    git-dev 第一次提交

--- src/main/java/com/zDing/test/Test01.java
+++ src/main/java/com/zDing/test/Test01.java
@@ -4,6 +4,10 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
         System.out.println("hello git2!!!");
+        System.out.println("hello git3!!!");
+        System.out.println("dev !!!");
+        System.out.println("hello git4 !!!");
+        System.out.println("hello git5 !!!");
     }
 
 }
